5eb369044b4d4e85807f53fc8342f661d351c714
ShiraOhana <soshiraohana@gmail.com>
2022-04-27 14:37:59 +0300

    24.3

---
+++ 24.3-own methods/app.js
@@ -0,0 +1,22 @@
+// Instructions
+// 1. Build your own filter method with the help of prototypes
+// 2. Build your own find method with the help of prototypes
+// Extra
+// 3. Build your own reduce method with the help of
+// prototypes
+
+Array.prototype.filter = function () {
+  return " Couldn't filter anything";
+};
+
+console.log([2, 3, 4].filter());
+
+Array.prototype.find = function () {
+  return " Couldn't find anything";
+};
+console.log([2, 3, 4].find());
+
+Array.prototype.reduce = function () {
+  return " Couldn't reduce anything";
+};
+console.log([2, 3, 4].reduce());
