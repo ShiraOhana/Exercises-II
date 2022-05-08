f4350999e623bcac259513135b1d5e4308c9b437
ShiraOhana <soshiraohana@gmail.com>
2022-05-08 23:40:52 +0300

    25.3

---
+++ 25.3-heroes/app.js
@@ -0,0 +1,13 @@
+const wonderWoman = {
+  name: "Diana Prince",
+};
+const batman = {
+  name: "Bruce Wayne",
+};
+const superHeroes = [wonderWoman, batman];
+function printName() {
+  console.log(`my name is ${this.name}`);
+}
+function printHeroes(heroes, printFunc) {
+  printName.bind();
+}
