c98eb41e0ad845b28738f202c5bcb772da7fd89d
ShiraOhana <soshiraohana@gmail.com>
2022-05-10 16:18:22 +0300

    30.1

---
+++ 30.1-github_api/app.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector("button");
+const avatar = document.querySelector("img");
+const user = document.querySelector(".user-name");
+const repo = document.querySelector(".num-of-repo");
+const username = document.querySelector(".search-bar");
+const link = document.querySelector("a");
+
+const getUser = async () => {
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/${username.value}`
+    );
+    const data = await response.json();
+    console.log(data);
+    avatar.src = data.avatar_url;
+    link.href = data.user.textContent = `Username: ${data.login}`;
+    repo.textContent = `Number of public repo: ${data.public_repos}`;
+  } catch (e) {
+    console.log(e);
+  }
+};
+btn.addEventListener("click", getUser);
